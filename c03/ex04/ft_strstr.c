71a0b27e4566d4f6917cdb8b060d2035452a4bbf
ldy6740 <52596117+ldy6740@users.noreply.github.com>
2020-11-01 21:01:45 +0900

    c03_201101 commit

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,12 +6,32 @@
 /*   By: dolim <dolim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 06:22:20 by dolim             #+#    #+#             */
-/*   Updated: 2020/11/01 06:34:46 by dolim            ###   ########.fr       */
+/*   Updated: 2020/11/01 17:42:48 by dolim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*ft_strstr(char *src, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
+	int i;
+	int j;
 
+	if (*to_find == 0)
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (to_find[j] == str[i +j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolim <dolim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/01 17:47:33 by dolim             #+#    #+#             */
+/*   Updated: 2020/11/01 20:59:16 by dolim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int c;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	j = 0;
+	while (dest[j])
+		j++;
+	c = 0;
+	while (src[c] && c + j < size - 1)
+	{
+		dest[j + c] = src[c];
+		c++;
+	}
+	if (c < size)
+		dest[c + j] = '\0';
+	if (size < j)
+		return (src[i] + size);
+	return (dest[j] + src[i]);
+}
+
+int		main(void)
+{
+	char dest[7];
+	char src[4];
+
+	dest[7] = "string";
+	src[4] = "src";
+	ft_strlcat(dest, src, 12);
+	return (0);
+}
