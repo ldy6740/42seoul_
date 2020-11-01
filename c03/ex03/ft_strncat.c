27811c1c09a304411c3712a5df2d8714ee37fd7d
ldy6740 <52596117+ldy6740@users.noreply.github.com>
2020-11-01 17:34:07 +0900

    c03 first commit

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolim <dolim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/01 05:12:44 by dolim             #+#    #+#             */
+/*   Updated: 2020/11/01 06:21:01 by dolim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+
+	while (*dest)
+		dest++;
+	i = 0;
+	while (*dest && i < nb)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
